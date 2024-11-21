c994068b486e4b0b82ab8a849f87a0fd7b7bbe02
platymeris <117546381+platymeris@users.noreply.github.com>
2024-11-21 17:42:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ def successor(n):
     return n + 1
 
 
+def id(*args):
+    return ([args[0], args[1]])
+
+
 def one(*args):
     return successor(zero())
 
 
 def projection(i):
-    def proj(args):
+    def proj(*args):
         return args[i - 1]
 
     return proj
@@ -24,203 +28,180 @@ def compose(f, *gs):
     return composed
 
 
+def substitution(g, *gs):
+    def substituted(*args):
+        results = [f(*args) for f in gs]
+        return g(*results)
+
+    return substituted
+
+
 def primitive_recursion(g, h):
-    def f(y, args):
+    def rec(y, *args):
         if y == 0:
-            return g(args)
+            return g(*args)
         else:
-            previous = f(y - 1, args)
-            return h(y - 1, previous, args)
+            previous = rec(y - 1, *args)
+            return h(y - 1, previous, *args)
 
-    return f
+    return rec
 
 
-def pred(args):
-    g = zero
+def two(*args):
+    return successor(successor(zero()))
 
-    def h(y_minus_1, previous, args):
-        return y_minus_1
 
-    pred_rec = primitive_recursion(g, h)
+def fifteen(*args):
+    return successor(successor(successor(successor(successor(
+        successor(successor(successor(successor(successor(successor(successor(successor(successor(zero()))))))))))))))
 
-    return pred_rec(args[0], args)
 
+def g_pred(*args):
+    return zero()
 
-def is_zero(args):
-    def g(args):
-        return one()
 
-    def h(y_minus_1, previous, args):
-        return zero()
+def h_pred(y_minus_1, previous, *args):
+    return y_minus_1
 
-    is_zero_rec = primitive_recursion(g, h)
-    return is_zero_rec(args[0], args)
 
+def pred(*args):
+    return substitution(primitive_recursion(g_pred, h_pred), projection(1))(*args)
 
-def is_less(args):
-    n, m = args[0], args[1]
 
-    sub_mn = subtract_example([m, n])
+def g_is_zero(*args):
+    return one()
 
-    is_zero_val = is_zero([sub_mn])
 
-    one = successor(zero(args))
-    is_less_val = subtract_example([one, is_zero_val])
-    return is_less_val
+def h_is_zero(y_minus_1, previous, *args):
+    return zero()
 
 
-def is_ge(args):
-    is_less_val = is_less(args)
-    one = successor(zero(args))
-    is_ge_val = subtract_example([one, is_less_val])
-    return is_ge_val
+def is_zero(*args):
+    return substitution(primitive_recursion(g_is_zero, h_is_zero), projection(1))(*args)
 
 
-def conditional(c, a, b, args):
-    part1 = multiply_example([c, a])
-    one = successor(zero(args))
-    one_minus_c = subtract_example([one, c])
-    part2 = multiply_example([one_minus_c, b])
-    new_val = add_example([part1, part2])
-    return new_val
+def g_subtract_example(*args):
+    return projection(1)(*args)
 
 
-def mod_example(args):
-    n, m = args[0], args[1]
+def h_subtract_example(y_minus_one, previous, *args):
+    return pred(previous, *args)
 
-    def g(args):
-        return n
 
-    def h(y_minus_1, previous, args):
-        c = is_ge([previous, m])
-        temp = subtract_example([previous, m])
-        new_remainder = conditional(c, temp, previous, args)
-        return new_remainder
+def subtract_example(*args):
+    return substitution(primitive_recursion(g_subtract_example, h_subtract_example), projection(2), projection(1))(
+        *args)
 
-    mod_rec = primitive_recursion(g, h)
-    return mod_rec(n, args)
 
+def g_add_example(*args):
+    return projection(1)(*args)
 
-def subtract_example(in_args):
-    g = projection(1)
 
-    def h(m_minus_1, previous, args):
-        return pred([previous])
+def h_add_example(y_minus_1, previous, *args):
+    return successor(previous)
 
-    sub = primitive_recursion(g, h)
 
-    return sub(in_args[1], in_args)
+def add_example(*args):
+    return substitution(primitive_recursion(g_add_example, h_add_example), projection(2), projection(1))(*args)
 
 
-def add_example(in_args):
-    g = projection(1)
+def g_multiply_example(*args):
+    return zero()
 
-    def h(m_minus_1, previous, args):
-        return successor(previous)
 
-    add_f = primitive_recursion(g, h)
-    return add_f(in_args[1], in_args)
+def h_multiply_example(*args):
+    return substitution(add_example, projection(3), projection(2))(*args)
 
 
-def multiply_example(in_args):
-    g = zero
+def multiply_example(*args):
+    return substitution(primitive_recursion(g_multiply_example, h_multiply_example), projection(2), projection(1))(
+        *args)
 
-    def h(m_minus_1, previous, args):
-        return add_example([projection(1)(args), previous])
 
-    mult_f = primitive_recursion(g, h)
-    return mult_f(in_args[1], in_args)
+def is_less(*args):
+    return substitution(subtract_example, one,
+                        substitution(is_zero, substitution(subtract_example, projection(2), projection(1))))(*args)
 
 
-def is_divisor(args):
-    n, k = args[0], args[1]
-    mod = mod_example([n, k])
-    return is_zero([mod])
+def is_ge(*args):
+    return substitution(subtract_example, one, is_less)(*args)
 
 
-def count_divisors_example(args):
-    n = args[0]
+def conditional(c, a, b, *args):
+    return add_example(multiply_example(c, a), multiply_example(subtract_example(one(), c), b))
 
-    def g(args):
-        return 0
 
-    def h(y_minus_1, previous, args):
-        y = successor(y_minus_1)
-        c = is_divisor([n, y])
-        return add_example([previous, c])
+def g_mod_example(*args):
+    return args[0]
 
-    count_div_rec = primitive_recursion(g, h)
-    return count_div_rec(n, [n])
 
+def h_mod_example(*args):
+    return substitution(conditional, substitution(is_ge, projection(2), projection(4)),
+                        substitution(subtract_example, projection(2), projection(4)), projection(2))(*args)
 
-def is_prime_example(args):
-    n = args[0]
 
-    count = count_divisors_example([n])
+def mod_example(*args):
+    return substitution(primitive_recursion(g_mod_example, h_mod_example), projection(1), projection(1), projection(2))(
+        *args)
 
-    def two(args):
-        return successor(successor(zero(args)))
 
-    count_minus_two = subtract_example([count, two([n])])
+def is_divisor(*args):
+    return substitution(is_zero, mod_example)(*args)
 
-    is_prime = is_zero([count_minus_two])
 
-    return is_prime
+def g_count_divisors_example(*args):
+    return zero()
 
 
-def count_primes_up_to_example(args):
-    y_max, n = args[0], args[1]
+def h_count_divisors_example(*args):
+    return substitution(add_example, projection(2),
+                        substitution(is_divisor, projection(3), substitution(successor, projection(1))))(*args)
 
-    def g(args):
-        return 0
 
-    def h(y_minus_1, previous, args):
-        y = successor(y_minus_1)
-        is_prime = is_prime_example([y])
-        return add_example([previous, is_prime])
+def count_divisors_example(*args):
+    return substitution(primitive_recursion(g_count_divisors_example, h_count_divisors_example), projection(1),
+                        projection(1))(*args)
 
-    count_primes_rec = primitive_recursion(g, h)
-    return count_primes_rec(y_max - 1, [y_max, n])
 
+def is_prime_example(*args):
+    return substitution(is_zero, substitution(subtract_example, count_divisors_example, two))(*args)
 
-def n_th_prime_example(args):
-    n = add_example([args[0], one()])
 
-    y_max_initial = 2
+def g_count_primes_up_to_example(*args):
+    return zero()
 
-    y_max_limit = multiply_example([n, 15])
 
-    def y_max_iteration(y_max, args):
-        count = count_primes_up_to_example([y_max, n])
+def h_count_primes_up_to_example(y_minus_one, previous, *args):
+    return add_example(previous, is_prime_example(successor(y_minus_one)))
 
-        c = is_ge([count, n])
 
-        y_max_plus_one = successor(y_max)
+def count_primes_up_to_example(*args):
+    return substitution(primitive_recursion(g_count_primes_up_to_example, h_count_primes_up_to_example),
+                        substitution(subtract_example, projection(1), one))(*args)
 
-        return conditional(c, y_max, y_max_plus_one, [y_max, n])
 
-    def find_nth_prime(y_max, args):
-        def g(args):
-            return y_max_initial
+def g_n_th_prime(*args):
+    return two()
 
-        def h(y_minus_1, previous, args):
-            return y_max_iteration(previous, args)
 
-        find_prime_rec = primitive_recursion(g, h)
-        return find_prime_rec(y_max_limit - y_max_initial, [y_max_limit, n])
+def h_n_th_prime(*args):
+    return substitution(conditional,
+                        substitution(is_ge, substitution(count_primes_up_to_example, projection(2), projection(3)),
+                                     projection(3)), projection(2), substitution(successor, projection(2)))(*args)
 
-    y_max_found = find_nth_prime(y_max_limit, [n])
 
-    y_max_prime = subtract_example([y_max_found, one()])
-
-    return y_max_prime
+def n_th_prime_example(*args):
+    return substitution(subtract_example, substitution(primitive_recursion(g_n_th_prime, h_n_th_prime),
+                                                       substitution(multiply_example,
+                                                                    substitution(add_example, projection(1), one),
+                                                                    fifteen), projection(1)), one)(*args)
 
 
 if __name__ == "__main__":
     print("\nТест функции n_prime:")
-    print(f"1 = {n_th_prime_example([1])}")
-    print(f"2 = {n_th_prime_example([2])}")
-    print(f"3 = {n_th_prime_example([3])}")
-    print(f"4 = {n_th_prime_example([4])}")
-    print(f"5 = {n_th_prime_example([5])}")
-    print(f"6 = {n_th_prime_example([6])}")
+    print(f"1 = {n_th_prime_example(1)}")
+    print(f"2 = {n_th_prime_example(2)}")
+    print(f"3 = {n_th_prime_example(3)}")
+    print(f"4 = {n_th_prime_example(4)}")
+    print(f"5 = {n_th_prime_example(5)}")
+    print(f"6 = {n_th_prime_example(6)}")
